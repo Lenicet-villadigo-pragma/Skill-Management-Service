dbd41434bb44f46a248425132e15ded254ab4997
Lenicet Villadiego Ricardo <lenicet.villadiego@pragma.com.co>
2026-02-16 14:41:48 -0500

    us3 unit test

--- skill-management-service-infrastructure/src/test/java/reactivechallenge/pragma/skillmanagementservice/out/adapter/SkillRepositoryImplTest.java
+++ skill-management-service-infrastructure/src/test/java/reactivechallenge/pragma/skillmanagementservice/out/adapter/SkillRepositoryImplTest.java
@@ -6,11 +6,14 @@ import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.data.domain.Pageable;
 import reactivechallenge.pragma.skillmanagementservice.mapper.DatabaseErrorMapper;
 import reactivechallenge.pragma.skillmanagementservice.mapper.SkillEntityMapper;
 import reactivechallenge.pragma.skillmanagementservice.mapper.SkillTechnologyEntityMapper;
 import reactivechallenge.pragma.skillmanagementservice.model.SkillModel;
 import reactivechallenge.pragma.skillmanagementservice.model.TechnologyExternalModel;
+import reactivechallenge.pragma.skillmanagementservice.model.criteria.SkillSortField;
+import reactivechallenge.pragma.skillmanagementservice.model.criteria.SkillSortOrder;
 import reactivechallenge.pragma.skillmanagementservice.out.entity.SkillEntity;
 import reactivechallenge.pragma.skillmanagementservice.out.entity.SkillTechnologyEntity;
 import reactivechallenge.pragma.skillmanagementservice.out.repository.ISkillRepository;
@@ -21,10 +24,8 @@ import reactor.test.StepVerifier;
 
 import java.util.List;
 
-import static org.mockito.ArgumentMatchers.any;
-import static org.mockito.ArgumentMatchers.anyList;
-import static org.mockito.Mockito.verify;
-import static org.mockito.Mockito.when;
+import static org.mockito.ArgumentMatchers.*;
+import static org.mockito.Mockito.*;
 
 @ExtendWith(MockitoExtension.class)
 class SkillRepositoryImplTest {
@@ -68,5 +69,87 @@ class SkillRepositoryImplTest {
                  .verifyComplete();
          verify(skillRepositoryMock).save(skillEntityToBeSaved);
      }
+
+     @Test
+     @DisplayName("get skills goes well")
+    void getSkillSuccess(){
+         // Arrange
+         Flux<SkillTechnologyEntity> fluxSkillTechnologyEntities = Flux.just(
+                 new SkillTechnologyEntity(1L,1L)
+                 , new SkillTechnologyEntity(2L,2L)
+                 , new SkillTechnologyEntity(3L,3L)
+         );
+         List<TechnologyExternalModel> technologyExternalModelList = List.of(
+                 new TechnologyExternalModel(1L,"")
+                 , new TechnologyExternalModel(2L,"")
+                 , new TechnologyExternalModel(3L,"")
+         );
+         SkillEntity skillEntity = new SkillEntity(1L,"name", "description", 3);
+         SkillModel skillModel = new SkillModel(1L,"name", "description", technologyExternalModelList);
+         Flux<SkillEntity> skillEntityFlux = Flux.just(skillEntity);
+
+         when(skillRepositoryMock.findAllBy(any(Pageable.class))).thenReturn(skillEntityFlux);
+         when(skillTechnologyRepositoryMock.findAllBySkillId(anyLong())).thenReturn(fluxSkillTechnologyEntities);
+
+         // Act
+         Flux<SkillModel> response = skillRepositoryImpl.getSkills(SkillSortField.NAME, SkillSortOrder.ASC,0,10);
+
+         // Assert
+         StepVerifier.create(response)
+                 .expectNext(skillModel)
+                 .verifyComplete();
+         verify(skillRepositoryMock).findAllBy(any(Pageable.class));
+         verify(skillTechnologyRepositoryMock).findAllBySkillId(anyLong());
+     }
+
+    @Test
+    @DisplayName("get skills goes well even when one of the skill have not list of skill")
+    void getSkillSuccessPartially(){
+        // Arrange
+        Flux<SkillTechnologyEntity> fluxSkillTechnologyEntities = Flux.just(
+                new SkillTechnologyEntity(1L,1L)
+                , new SkillTechnologyEntity(2L,2L)
+                , new SkillTechnologyEntity(3L,3L)
+        );
+        List<TechnologyExternalModel> technologyExternalModelList = List.of(
+                new TechnologyExternalModel(1L,"")
+                , new TechnologyExternalModel(2L,"")
+                , new TechnologyExternalModel(3L,"")
+        );
+        SkillEntity skillEntity1 = new SkillEntity(1L,"name", "description", 3);
+        SkillEntity skillEntity2 = new SkillEntity(2L,"name2", "description2", 0);
+        SkillModel skillModel = new SkillModel(1L,"name", "description", technologyExternalModelList);
+        Flux<SkillEntity> skillEntityFlux = Flux.just(skillEntity1, skillEntity2);
+
+        when(skillRepositoryMock.findAllBy(any(Pageable.class))).thenReturn(skillEntityFlux);
+        when(skillTechnologyRepositoryMock.findAllBySkillId(anyLong())).thenReturn(fluxSkillTechnologyEntities).thenReturn(Flux.empty());
+
+        // Act
+        Flux<SkillModel> response = skillRepositoryImpl.getSkills(SkillSortField.NAME, SkillSortOrder.ASC,0,10);
+
+        // Assert
+        StepVerifier.create(response)
+                .expectNext(skillModel)
+                .verifyComplete();
+        verify(skillRepositoryMock).findAllBy(any(Pageable.class));
+        verify(skillTechnologyRepositoryMock, times(2)).findAllBySkillId(anyLong());
+    }
+
+    @Test
+    @DisplayName("countSkills returns correctly")
+    void countSkillsTest() {
+         // Arrange
+        Mono<Long> actual = null;
+       Long expected = 1L;
+
+       when(skillRepositoryMock.count()).thenReturn(Mono.just(1L));
+
+        // Arrange
+        actual = skillRepositoryImpl.countSkills();
+
+        // Assert
+        StepVerifier.create(actual).expectNext(expected).verifyComplete();
+        verify(skillRepositoryMock).count();
+    }
 
 }
--- skill-management-service-infrastructure/src/test/java/reactivechallenge/pragma/skillmanagementservice/out/adapter/TechnologyServiceAdapterTest.java
+++ skill-management-service-infrastructure/src/test/java/reactivechallenge/pragma/skillmanagementservice/out/adapter/TechnologyServiceAdapterTest.java
@@ -8,6 +8,8 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.web.reactive.function.client.WebClient;
 import reactivechallenge.pragma.skillmanagementservice.exception.InconsistencyDataException;
+import reactivechallenge.pragma.skillmanagementservice.model.TechnologyExternalModel;
+import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
 import reactor.test.StepVerifier;
 
@@ -44,11 +46,12 @@ class TechnologyServiceAdapterTest {
         mockWebServer.enqueue(new MockResponse()
                 .setBody("true")
                 .addHeader("Content-Type", "application/json"));
-
         List<String> techIds = Arrays.asList("1", "2", "3");
+
+        // Act
         Mono<Boolean> result = technologyServiceAdapter.existsById(techIds);
 
-        // Act & Assert
+        // Assert
         StepVerifier.create(result)
                 .expectNext(true)
                 .verifyComplete();
@@ -60,11 +63,12 @@ class TechnologyServiceAdapterTest {
         mockWebServer.enqueue(new MockResponse()
                 .setBody("false")
                 .addHeader("Content-Type", "application/json"));
-
         List<String> techIds = Arrays.asList("1", "2", "3");
+
+        // Act
         Mono<Boolean> result = technologyServiceAdapter.existsById(techIds);
 
-        // Act & Assert
+        // Assert
         StepVerifier.create(result)
                 .expectNext(false)
                 .verifyComplete();
@@ -77,11 +81,12 @@ class TechnologyServiceAdapterTest {
                 .setBody("true")
                 .addHeader("Content-Type", "application/json")
                 .setBodyDelay(6, TimeUnit.SECONDS)); // Delay longer than timeout
-
         List<String> techIds = Arrays.asList("1", "2", "3");
+
+        // Act
         Mono<Boolean> result = technologyServiceAdapter.existsById(techIds);
 
-        // Act & Assert
+        // Assert
         StepVerifier.create(result)
                 .expectError(InconsistencyDataException.class)
                 .verify();
@@ -91,13 +96,55 @@ class TechnologyServiceAdapterTest {
     void existsById_shouldThrowInconsistencyDataException_on5xxError() {
         // Arrange
         mockWebServer.enqueue(new MockResponse().setResponseCode(500));
-
         List<String> techIds = Arrays.asList("1", "2", "3");
+
+        // Act
         Mono<Boolean> result = technologyServiceAdapter.existsById(techIds);
 
         // Act & Assert
         StepVerifier.create(result)
                 .expectError(InconsistencyDataException.class)
                 .verify();
+    }
+
+    @Test
+    void getTechsByIds_returns_elements_when_there_arent_error() {
+        // Arrange
+        TechnologyExternalModel technologyExternalModel =  new TechnologyExternalModel(1L, "name");
+        String mockResponse = " [ "
+                + "{\"id\": 1, \"name\": \"name\"}"
+                +"]";
+        mockWebServer.enqueue(new MockResponse()
+                .setBody(mockResponse)
+                .addHeader("Content-Type", "application/json")
+        );
+        List<String> techIds = Arrays.asList("1", "2", "3");
+
+        // Act
+        Flux<TechnologyExternalModel> technologyExternalModelFlux = technologyServiceAdapter.getTechsByIds(techIds);
+
+
+        // Arrange & Assert
+        StepVerifier.create(technologyExternalModelFlux)
+                .expectNext(technologyExternalModel)
+                .verifyComplete();
+
+    }
+
+    @Test
+    void getTechsByIds_returns_empty_because_there_are_error() {
+        // Arrange
+        mockWebServer.enqueue(new MockResponse().setResponseCode(500));
+        List<String> techIds = Arrays.asList("1", "2", "3");
+
+        // Act
+        Flux<TechnologyExternalModel> technologyExternalModelFlux = technologyServiceAdapter.getTechsByIds(techIds);
+
+
+        // Arrange & Assert
+        StepVerifier.create(technologyExternalModelFlux)
+                .expectNextCount(0)
+                .verifyComplete();
+
     }
 }
