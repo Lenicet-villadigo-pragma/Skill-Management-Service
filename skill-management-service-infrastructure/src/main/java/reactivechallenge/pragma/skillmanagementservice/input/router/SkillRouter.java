e3ff335c145c613dc1e86664140bf213bfa61b2a
Lenicet Villadiego Ricardo <lenicet.villadiego@pragma.com.co>
2026-02-17 21:46:03 -0500

    merge us3 into us4. list skill, change path variable for query param

--- skill-management-service-infrastructure/src/main/java/reactivechallenge/pragma/skillmanagementservice/input/router/SkillRouter.java
+++ skill-management-service-infrastructure/src/main/java/reactivechallenge/pragma/skillmanagementservice/input/router/SkillRouter.java
@@ -119,8 +119,7 @@ public class SkillRouter {
     })
     public RouterFunction<ServerResponse> skillRoutes(SkillHandler skillHandler) {
         return route(POST("/create").and(accept(MediaType.APPLICATION_JSON)), skillHandler::createSkill)
-                .andRoute(GET("/retrieve"), skillHandler::listSkills);
-                .andRoute(GET("/retrieve/sortField/{sortField}/sortOrder/{sortOrder}/pageSize/{pageSize}/pageNumber/{pageNumber}"), skillHandler::listSkills)
+                .andRoute(GET("/retrieve"), skillHandler::listSkills)
                 .andRoute(GET("/exists"), skillHandler::verifyIfSkillsExists);
     }
 }
